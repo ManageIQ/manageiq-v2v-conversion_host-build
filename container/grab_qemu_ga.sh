c79d08338fe032b50e54c339dd8b0b6e5df3495f
Satoe Imaishi <simaishi@redhat.com>
2020-04-28 09:00:29 -0400

    Fix el8 yum repo path for qemu-guest-agent
    (cherry picked from commit 3a1394e9d94d05ed59c2669c1ad8d11fd05f799b)

--- container/grab_qemu_ga.sh
+++ container/grab_qemu_ga.sh
@@ -5,7 +5,7 @@ set -e
 declare -A location=(
     ['el6']='http://vault.centos.org/6.9/os/x86_64/Packages/'
     ['el7']='http://vault.centos.org/7.6.1810/os/x86_64/Packages/'
-    ['el8']='http://mirror.centos.org/centos-8/8.0.1905/AppStream/x86_64/os/Packages/'
+    ['el8']='http://mirror.centos.org/centos-8/8/AppStream/x86_64/os/Packages/'
     ['fc28']='http://ftp.fi.muni.cz/pub/linux/fedora/linux/releases/30/Everything/x86_64/os/Packages/q/'
     ['lp151']='http://download.opensuse.org/distribution/leap/15.1/repo/oss/x86_64/'
     )
